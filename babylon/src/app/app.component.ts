f8de72f2d1aa4880966f61dc3e2ee101f828ef1a
rosyk <rosyknikita@gmail.com>
2023-10-23 17:42:54 +0300

    redisign home page

--- babylon/src/app/app.component.ts
+++ babylon/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'babylon';
+  isHome = false;
+  isLessons = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        const contentElement = document.querySelector('.content') as HTMLElement;
+        if (event.url === '/Lessons') {
+          // if (contentElement !== null) {
+          //   contentElement.style.background = 'white';
+          // }
+          this.isLessons = true;
+          this.isHome = false;
+        }
+
+        if (event.url === '/Home') {
+          // if (contentElement !== null) {
+          //   contentElement.style.background = 'linear-gradient(90deg, rgba(79,37,28,0.6) 0%, rgba(79,37,28,0.6) 100%), url(\'/assets/img/home-page/bg.jpg\') no-repeat';
+          //   contentElement.style.backgroundSize = 'cover'
+          // }
+          this.isHome = true;
+          this.isLessons = false;
+        }
+      }
+    });
+  }
 }
