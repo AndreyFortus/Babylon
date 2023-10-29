307c8fdf2db158f717aee3adba68b31cfd1dc8de
rosyk <rosyknikita@gmail.com>
2023-10-29 11:37:49 +0200

    add quizz page

--- babylon/src/app/app-routing.module.ts
+++ babylon/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { LessonsPageComponent } from './lessons-page/lessons-page.component';;
+import { LessonsPageComponent } from './lessons-page/lessons-page.component'
+import { LessonStartQuizSectionComponent } from './lesson-start-quiz-section/lesson-start-quiz-section.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/Home', pathMatch: 'full' }, // Это перенаправление на /home}
   { path: 'Home', component: HomePageComponent },
-  { path: 'Lessons', component: LessonsPageComponent }
+  { path: 'Lessons', component: LessonsPageComponent },
+  { path: 'Lesson/:id/quizz', component: LessonStartQuizSectionComponent},
 ];
 
 @NgModule({
