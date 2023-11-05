9fb1a196bd1c19ab9fd7ebeb3496fd5dfb10e75c
rosyk <rosyknikita@gmail.com>
2023-11-05 13:12:18 +0200

    remove backend from branch

--- backend/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import LevelUpdateAPIView, UserDetailView
-
-
-urlpatterns = [
-    path('users/<int:pk>/detail/', UserDetailView.as_view(), name='user-detail'),
-    path('get-user-info/', UserDetailView.as_view(), name='get-user-info'),
-    path('user-update-level/', LevelUpdateAPIView.as_view(), name='level-update'),
-]
