19f2089e9a2bb5a7df03ea8ea7dbf1a43a1f8724
Andrey <andrey020904@gmail.com>
2023-11-15 13:49:28 +0200

    add user validation for entering the chat

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -9,17 +10,59 @@ from .serializers import MessageSerializer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    @database_sync_to_async
+    def validate_user(self, token_key, initiator_id, receiver_id):
+        try:
+            token = Token.objects.get(key=token_key)
+            user = token.user
+
+            if user.id not in [initiator_id, receiver_id]:
+                raise ValueError("Access denied")
+
+            return user
+        except Token.DoesNotExist:
+            raise ValueError("Invalid token")
+
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
+        token_key = self.scope.get("query_string", b"").decode("utf-8").split("=Token%20")[1]
 
-        # Join room group
+        conversation_data = await self.get_conversation_data()
+
+        initiator_id = conversation_data['initiator']['id']
+        receiver_id = conversation_data['receiver']['id']
+
+        try:
+            user = await self.validate_user(token_key, initiator_id, receiver_id)
+        except ValueError as e:
+            await self.close()
+            return
+
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
         await self.accept()
+
+    async def get_conversation_data(self):
+        return await self.get_conversation_data_instance()
+
+    @database_sync_to_async
+    def get_conversation_data_instance(self):
+        conversation_id = re.search(r'\d+', self.room_name).group()
+        conversation = Conversation.objects.get(id=int(conversation_id))
+
+        conversation_data = {
+            'initiator': {
+                'id': conversation.initiator.id,
+            },
+            'receiver': {
+                'id': conversation.receiver.id,
+            },
+        }
+        return conversation_data
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -49,12 +92,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def chat_message(self, event):
-        # Отправка сообщения всем клиентам в группе
         message_data = {
             "id": event["message"]["id"],
             "text": event["message"]["text"],
             "timestamp": event["message"]["timestamp"],
-            "sender": str(event["message"]["sender"]),  # Преобразование sender_id в строку
+            "sender": str(event["message"]["sender"]),
         }
         await self.send(text_data=json.dumps(message_data))
 
