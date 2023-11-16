ad3b85c3e0b8e7bd881e4b9da0d1b842d7073809
Andrey <andrey020904@gmail.com>
2023-11-16 20:48:22 +0200

    code improve

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -14,26 +14,20 @@ from rest_framework.response import Response
 @permission_classes([IsAuthenticated])
 def start_convo(request):
     username = request.user.username
-
-    if not username:
-        return Response({'message': 'Invalid request data'}, status=400)
-
     participant_username = request.data.get('username')
 
-    if not participant_username:
-        return Response({'message': 'Invalid request data'}, status=400)
+    if participant_username and username:
+        participant = get_object_or_404(User, username=participant_username)
+        conversation = Conversation.objects.filter(Q(initiator=request.user, receiver=participant) |
+                                                   Q(initiator=participant, receiver=request.user)).first()
 
-    participant = get_object_or_404(User, username=participant_username)
+        if conversation:
+            return Response({'conversation_id': conversation.id})
 
-    conversation = Conversation.objects.filter(Q(initiator=request.user, receiver=participant) |
-                                               Q(initiator=participant, receiver=request.user)).first()
-
-    if conversation:
-        return Response({'conversation_id': conversation.id})
-    else:
         conversation = Conversation.objects.create(initiator=request.user, receiver=participant)
         serializer = ConversationSerializer(instance=conversation, context={'request': request})
         return Response(serializer.data, status=201)
+    return Response({'message': 'Invalid request data'}, status=400)
 
 @api_view(['GET'])
 def get_conversation(request, convo_id):
