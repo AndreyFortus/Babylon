e8a01da428eb4cc929b9fc0cf78ee72ab5f0ea5e
Andrey <andrey020904@gmail.com>
2023-11-30 21:11:04 +0200

    add test for websocket

--- backend/chat/tests_async.py
+++ backend/chat/tests_async.py
@@ -1,29 +1,51 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'babylon_project.settings')
+django.setup()
+
 import json
 import time
+import unittest
 import websocket
+from .models import Message
 
-url = "ws://127.0.0.1:8080/ws/chat/2/"
-token = "b712ed6d26e50d86d852557ee33c222b3fc4af95"
+class WebSocketTest(unittest.TestCase):
+    def setUp(self):
+        self.token = "b712ed6d26e50d86d852557ee33c222b3fc4af95"
+        self.url = f"ws://127.0.0.1:8080/ws/chat/1/?protocol=Token%20{self.token}"
+        self.created_message_id = None
 
-headers = {
-    "Sec-WebSocket-Protocol": f"Token {token}"
-}
+    def tearDown(self):
+        # Delete the created object from the database
+        if self.created_message_id is not None:
+            Message.objects.filter(id=self.created_message_id).delete()
 
-ws = websocket.create_connection(url, header=headers)
+    def test_websocket_connection(self):
+        ws = websocket.create_connection(self.url)
 
-try:
-    message_data = {
-        "message": "Hello, WebSocket!"
-    }
+        try:
+            message_data = {
+                "message": "Hello, WebSocket!"
+            }
 
-    message_json = json.dumps(message_data)
-    ws.send(message_json)
-    print(f"Sent: {message_json}")
+            message_json = json.dumps(message_data)
+            ws.send(message_json)
+            print(f"Sent: {message_json}")
 
-    time.sleep(10)
+            time.sleep(2)
 
-    response = ws.recv()
-    print(f"Received: {response}")
+            response = ws.recv()
+            print(f"Received: {response}")
 
-finally:
-    ws.close()
+            self.assertIn("Hello, WebSocket!", response)
+
+            # Capture the created object ID
+            self.created_message_id = json.loads(response)['id']
+
+        finally:
+            ws.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
