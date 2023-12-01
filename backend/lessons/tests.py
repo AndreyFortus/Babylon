c14a9858908a6459e19c797c38f6af2f5af1cab6
Andrey <andrey020904@gmail.com>
2023-12-01 20:32:14 +0200

    add test for fill the blank questions

--- backend/lessons/tests.py
+++ backend/lessons/tests.py
@@ -92,3 +92,50 @@ class MultipleChoiceQuestionListViewTest(TestCase):
         # Check for the expected status code
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertGreaterEqual(len(response.data), 0)
+
+
+class FillBlankQuestionListViewTest(TestCase):
+    def setUp(self):
+        self.lesson = Lesson.objects.create(lesson_title='Test Lesson', hp=100)
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+
+        FillBlankQuestion.objects.create(
+            lesson=self.lesson,
+            question='Test FillBlank Question',
+            correct_answers_fill='Answer 1;Answer 2;Answer 3'
+        )
+
+    @patch('lessons.views.check_user_level')
+    def test_fill_blank_list_view_authenticated(self, mock_check_user_level):
+        # Mocking the check_user_level function to always return True (for access)
+        mock_check_user_level.return_value = True
+
+        # Authenticate the user
+        self.client.force_authenticate(user=self.user, token=self.token)
+
+        # Make a request to the API endpoint
+        response = self.client.get(f'/api/lesson/{self.lesson.pk}/questions/fill-blank/')
+
+        # Check for the expected status code
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertGreaterEqual(len(response.data), 1)
+        self.assertIn('question', response.data[0])
+        self.assertIn('correct_answers_fill', response.data[0])
+
+    @patch('lessons.views.check_user_level')
+    def test_fill_blank_list_view_unauthenticated(self, mock_check_user_level):
+        # Mocking the check_user_level function to always return True (for access)
+        mock_check_user_level.return_value = True
+
+        # Clear any existing credentials
+        self.client.credentials()
+
+        # Make a request to the API endpoint
+        response = self.client.get(f'/api/lesson/{self.lesson.pk}/questions/fill-blank/')
+
+        # Check for the expected status code
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertGreaterEqual(len(response.data), 0)
